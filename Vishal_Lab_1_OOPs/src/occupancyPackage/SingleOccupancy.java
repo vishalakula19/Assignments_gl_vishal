dedb402d69e9a49650ba2cf5bf0fd26e569e339c
vishalakula19 <vishalakula19@gmail.com>
2022-09-24 02:53:20 +0530

    Delete Vishal_Lab_1_OOPs directory

--- Vishal_Lab_1_OOPs/src/occupancyPackage/SingleOccupancy.java
+++
@@ -1,8 +0,0 @@
-package occupancyPackage;
-public class SingleOccupancy extends Occupancy {
-
-    public SingleOccupancy() {
-        this.price = 2000.00;
-        this.name = "Single Occupancy";
-    }
-}
--- Vishal_Lab_1_OOPs/src/occupancyPackage/TripleOccupancy.java
+++
@@ -1,8 +0,0 @@
-package occupancyPackage;
-public class TripleOccupancy extends Occupancy {
-
-    public TripleOccupancy() {
-        this.price = 4000.00;
-        this.name = "Triple Occupancy";
-    }
-}
