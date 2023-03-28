0c6d1f655736608f39d30cd20b646f5602eab9e4
vishalakula19 <vishalakula19@gmail.com>
2023-03-28 12:41:58 +0530

    Delete Vishal_Lab_1_OOPs directory

--- Vishal_Lab_1_OOPs/occupancyPackage/DoubleOccupancy.java
+++
@@ -1,8 +0,0 @@
-package occupancyPackage;
-public class DoubleOccupancy extends Occupancy {
-
-    public DoubleOccupancy() {
-        this.price = 3000.00;
-        this.name = "Double Occupancy";
-    }
-}
